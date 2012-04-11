4ddedd80c01a2baddb730784a87268e92fdb75c1
Dan LaManna <dan.lamanna@gmail.com>
2012-04-11 04:39:52 -0400

    Adding ido config, saveplace implementation, and browse kill ring.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -7,7 +7,7 @@
 "Directory containing Emacs libraries required for personal setup.")
 (add-to-list 'load-path shared-lisp-dir)
 
-;; Load Aesthetics 
+;; Load Aesthetics
 (add-to-list 'load-path "~/.emacs.d/lib/aesthetics/color-theme")
 (load "color-theme.el")
 (load "themes/color-theme-almost-monokai.el")
@@ -80,3 +80,34 @@
 	   ;; Display time, followed by dashes till the end
 	    '(:eval (propertize (format-time-string "%l:%M%p")))
 	    " %-"))
+
+(load "browse-kill-ring.el")
+(when (require 'browse-kill-ring nil 'noerror)
+  (browse-kill-ring-default-keybindings))
+
+(setq save-place-file "~/.emacs.d/saveplace")
+(setq-default save-place t)
+(require 'saveplace)
+
+(require 'ido) 
+(ido-mode 'both) ; for buffers and files
+(setq 
+  ido-save-directory-list-file "~/.emacs.d/cache/ido.last"
+
+  ido-ignore-buffers
+  '("\\` " "^\*Mess" "^\*Back" ".*Completion" "^\*Ido" "^\*trace"
+
+     "^\*compilation" "^\*GTAGS" "^session\.*" "^\*")
+  ido-work-directory-list '("~/" "~/Desktop" "~/Documents" "~src")
+  ido-case-fold  t                 ; be case-insensitive
+
+  ido-enable-last-directory-history t ; remember last used dirs
+  ido-max-work-directory-list 30   ; should be enough
+  ido-max-work-file-list      50   ; remember many
+  ido-use-filename-at-point nil    ; don't use filename at point (annoying)
+  ido-use-url-at-point nil         ; don't use url at point (annoying)
+  ido-max-prospects 10              ; don't spam my minibuffer
+  ido-confirm-unique-completion nil)
+
+; when using ido, the confirmation is rather annoying...
+ (setq confirm-nonexistent-file-or-buffer nil)
