dc3ea2d933e8956bb2eb34ee4e1a0ce4c6990cf7
Dan LaManna <dan.lamanna@gmail.com>
2012-07-31 21:45:23 -0400

    Adding aesthetics config.

---
+++ .emacs.d/config/aesthetics-config.el
@@ -0,0 +1,8 @@
+(add-to-list 'load-path "~/.emacs.d/lib/aesthetics/color-theme")
+
+(require 'color-theme)
+(load "~/.emacs.d/lib/aesthetics/color-theme/themes/color-theme-almost-monokai.el")
+(color-theme-almost-monokai)
+
+
+(provide 'aesthetics-config)
