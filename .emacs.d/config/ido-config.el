adf82eb472751a95eaf6c1e756aaca44d2735fbc
Dan LaManna <dan.lamanna@gmail.com>
2012-08-14 07:49:21 -0400

    Adding ido-create-new-buffer setting.

--- .emacs.d/config/ido-config.el
+++ .emacs.d/config/ido-config.el
@@ -3,10 +3,7 @@
 (setq 
   ido-save-directory-list-file "~/.emacs.d/cache/ido.last"
 
-  ido-ignore-buffers
-  '("\\` " "^\*Mess" "^\*Back" ".*Completion" "^\*Ido" "^\*trace"
-
-     "^\*compilation" "^\*GTAGS" "^session\.*" "^\*")
+  ido-ignore-buffers '("\\` " "^\*Mess" "^\*Back" ".*Completion" "^\*Ido" "^\*trace" "^\*compilation" "^\*GTAGS" "^session\.*" "^\*")
   ido-work-directory-list '("~/" "~/Desktop" "~/Documents" "~src")
   ido-case-fold  t                 ; be case-insensitive
 
@@ -16,6 +13,7 @@
   ido-use-filename-at-point nil    ; don't use filename at point (annoying)
   ido-use-url-at-point nil         ; don't use url at point (annoying)
   ido-max-prospects 10              ; don't spam my minibuffer
+  ido-create-new-buffer 'always ;don't prompt if a buff er doesn't exist, just create
   ido-confirm-unique-completion nil)
 
 ; when using ido, the confirmation is rather annoying...
