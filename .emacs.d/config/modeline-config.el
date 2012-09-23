23d16d73cfbf95befcac173e8b3e525aba4df9cc
Dan LaManna <dan.lamanna@gmail.com>
2012-09-22 23:40:12 -0400

    Fixing modeline config to only show buffer-modified when appropriate.

--- .emacs.d/config/modeline-config.el
+++ .emacs.d/config/modeline-config.el
@@ -1,15 +1,17 @@
 (setq default-mode-line-format
-          (list
+	  (list
 	   "-- "
 	   ;; Displays buffer name bolded
 	   '(:eval (propertize "%b" 'face 'bold 'help-echo (buffer-name)))
-	   ;; Displays ** bolded if the file has been modified
-           '(:eval (when (buffer-modified-p)
+	   ;; Displays ** bolded if the file has been modified (and it's not a readonly buffer)
+	   '(:eval (when (and (buffer-modified-p) (eq buffer-read-only nil) (not (eq (buffer-file-name) nil)))
 		     (propertize "**" 'face 'bold)))
 	   ;; Display percent from top, then line num, col num
-           "  %p (L%l,C%c)"
+	   '(:eval (if (not (eq (buffer-file-name) nil))
+		       "  %p (L%l,C%c)"))
 	   ;; Major mode in brackets
 	   " [%m]"
+	   ;'(:eval (propertize (cdr (get-current-project buffer-file-name)) 'face 'bold))
 	   ;; Display time, followed by dashes till the end
 	    '(:eval (propertize (format-time-string "%l:%M%p")))
 	    " %-"))
