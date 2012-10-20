b4a24cefd9f722b52aa08111f134cff5a9504469
Dan LaManna <dan.lamanna@gmail.com>
2012-10-20 08:31:28 -0400

    Fixing includes, yasnippet dir, adding defadvice for zap to char.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,11 +4,16 @@
 (add-to-list 'load-path "~/.emacs.d/config/")
 (add-to-list 'load-path "~/.emacs.d/lib/yasnippet")
 (add-to-list 'load-path "~/.emacs.d/lib/expand-region")
+(add-to-list 'load-path "~/.emacs.d/lib/multiple-cursors")
 (add-to-list 'load-path "~/.emacs.d/lib/jabber")
 (add-to-list 'load-path "~/.emacs.d/lib/zencoding")
 (add-to-list 'load-path "~/.emacs.d/lib/magit")
 (add-to-list 'load-path "~/.emacs.d/lib/modes/js2-mode")
+(add-to-list 'load-path "~/.emacs.d/lib/modes/php-mode")
 (add-to-list 'load-path "~/.emacs.d/lib/js2-refactor.el")
+(add-to-list 'load-path "~/.emacs.d/lib/jquery-doc")
+(load "~/.emacs.d/lib/nxhtml/autostart.el")
+
 (add-to-list 'load-path "~/.emacs.d/lib/cedet-1.1.1/common")
 (add-to-list 'load-path "~/.emacs.d/lib/auto-complete-1.3.1")
 
@@ -24,6 +29,7 @@
 (require 'browse-kill-ring)
 (require 'keymaps)
 (require 'js2-refactor)
+(require 'jquery-doc)
 (require 'mark-multiple)
 (require 'inline-string-rectangle)
 (require 'mark-more-like-this)
@@ -34,7 +40,10 @@
 (require 'restclient)
 (require 'json-reformat)
 (require 'expand-region)
+(require 'multiple-cursors)
 (require 'uniquify)
+
+(setq yas-snippet-dirs '("~/.emacs.d/snippets"))
 (require 'yasnippet)
 (yas/global-mode 1)
 
@@ -47,3 +56,18 @@
 (require 'misc-config)
 (require 'twittering-mode-config)
 (require 'workgroups)
+
+
+;; takes a style tag, like "display:none;margin:5px;" and formats it into a css style
+
+
+;; enable auto-revert mode for jabber emacs
+
+(defadvice zap-to-char (after zap-until-char (arg char) activate)
+  "Makes zap-to-char act like zap-until-char."
+  (insert char)
+  (backward-char 1))
+
+(defun place-var-in-php-echo()
+  (interactive)
+  (replace-string (current-word) (format "<?php echo %s; ?>" (current-word)) t))
