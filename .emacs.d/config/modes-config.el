24f566d073ab0e2b22e8dadaaa61f9061a98f7d2
Dan LaManna <dan.lamanna@gmail.com>
2012-11-27 20:54:22 -0500

    Beefing up php mode with flymake/completions.

--- .emacs.d/config/modes-config.el
+++ .emacs.d/config/modes-config.el
@@ -1,15 +1,28 @@
 ; PHP
+(custom-set-variables
+ '(php-manual-path (expand-file-name (format "%s/etc/php-manual" emacs-config-dir)))
+ '(php-completion-file (expand-file-name (format "%s/etc/php-completion.txt" emacs-config-dir))))
+
 (autoload 'php-mode "php-mode" "Major mode for PHP." t)
 (add-to-list 'auto-mode-alist '("\\.\\(php\\|phtml\\)\\'" . php-mode))
 
+(require 'flymake)
+(add-hook 'php-mode-hook 'flymake-mode-on)
 
+(require 'wordpress-mode)
+(add-hook 'php-mode-hook '(lambda()
+			    (if (wp/exists)
+				(wordpress-mode))))
+
+(add-hook 'php-mode-hook '(lambda()
+			    (define-key php-mode-map (kbd "C-c C-f") 'php-search-local-documentation)))
+
+(defadvice yas--fallback (before try-php-expand (&optional arg from-trigger-key-p) activate)
+  (when (string-equal mode-name "PHP")
+    (let (message-log-max)
+      (php-complete-function))))
 
 ; C
-(add-hook 'c-mode-hook (lambda()
-			 (set (make-local-variable 'compile-command)
-			      (concat "gcc -o " (substring (format "%s" (buffer-name)) 0 (- (length (buffer-name)) 2)) " " (buffer-name))
-			      (concat "gcc -Wall -g -o " (substring (format "%s" (buffer-name)) 0 (- (length (buffer-name)) 2)) " " (buffer-name)))))
-
 (defun compile-or-recompile()
   (interactive)
   (if (get-buffer "*compilation*")
