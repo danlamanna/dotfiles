eda4272e6f4c79e62b979790360d4da326f2f7ac
dan <dan@sateda.(none)>
2012-12-14 05:34:04 -0500

    Disabling backup files, and the splash screen.

--- .emacs.d/config/emacs-config.el
+++ .emacs.d/config/emacs-config.el
@@ -3,9 +3,12 @@
 (show-paren-mode t)
 (setq show-paren-style 'mixed)
 
+(setq inhibit-splash-screen t)
+
 ;; Autosave/Backup Stuff
 (setq auto-save-list-file-prefix nil)
 (setq temporary-file-directory emacs-tmp-dir)
+(setq make-backup-files nil)
 
 ;; X Emacs stuff..
 (if (fboundp 'tool-bar-mode)
@@ -27,7 +30,7 @@
 
 ;; Dired
 (add-hook 'dired-mode-hook (lambda()
-			     (hl-line-mode)))
+                             (hl-line-mode)))
 
 ;; Winner mode
 (require 'winner)
@@ -43,16 +46,16 @@
 
 ;; Before save
 (add-hook 'before-save-hook '(lambda()
-			       (if (buffer-file-name)
-				   (unless (string-equal (file-name-extension (buffer-file-name)) "md")
-				     (whitespace-cleanup)))))
+                               (if (buffer-file-name)
+                                   (unless (string-equal (file-name-extension (buffer-file-name)) "md")
+                                     (whitespace-cleanup)))))
 (add-hook 'before-save-hook
-	  '(lambda ()
-	     "If the file doesn't exist, we attempt to make the directory,
-	      this way, we can 'open' files in non-existent directories, and when
-	      saving them, it creates the directory for us."
-	     (or (file-exists-p (file-name-directory buffer-file-name))
-		 (make-directory (file-name-directory buffer-file-name) t))))
+          '(lambda ()
+             "If the file doesn't exist, we attempt to make the directory,
+              this way, we can 'open' files in non-existent directories, and when
+              saving them, it creates the directory for us."
+             (or (file-exists-p (file-name-directory buffer-file-name))
+                 (make-directory (file-name-directory buffer-file-name) t))))
 
 (custom-set-variables
  '(browse-url-browser-function 'w3m-browse-url))
@@ -65,9 +68,9 @@
     kill-ring."
   (interactive)
   (let ((beg (line-beginning-position 1))
-	(end (line-beginning-position 2)))
+        (end (line-beginning-position 2)))
     (if (eq last-command 'quick-copy-line)
-	(kill-append (buffer-substring beg end) (< end beg))
+        (kill-append (buffer-substring beg end) (< end beg))
       (kill-new (buffer-substring beg end))))
   (beginning-of-line 2))
 
@@ -80,7 +83,7 @@
 ;; Remove prompt of killing a buffer with a running process
 (setq kill-buffer-query-functions
   (remq 'process-kill-buffer-query-function
-	 kill-buffer-query-functions))
+         kill-buffer-query-functions))
 
 ;; Misc functions
 (defun swap-windows()
@@ -88,16 +91,16 @@
  (interactive)
  (cond ((not (= (count-windows) 2)) (message "You need exactly 2 windows to do this."))
        (t
-	(let* ((w1 (first (window-list)))
-	       (w2 (second (window-list)))
-	       (b1 (window-buffer w1))
-	       (b2 (window-buffer w2))
-	       (s1 (window-start w1))
-	       (s2 (window-start w2)))
-	  (set-window-buffer w1 b2)
-	  (set-window-buffer w2 b1)
-	  (set-window-start w1 s2)
-	  (set-window-start w2 s1)))))
+        (let* ((w1 (first (window-list)))
+               (w2 (second (window-list)))
+               (b1 (window-buffer w1))
+               (b2 (window-buffer w2))
+               (s1 (window-start w1))
+               (s2 (window-start w2)))
+          (set-window-buffer w1 b2)
+          (set-window-buffer w2 b1)
+          (set-window-start w1 s2)
+          (set-window-start w2 s1)))))
 
 (defun rename-file-and-buffer(new-name)
  "Renames both current buffer and file it's visiting to NEW-NAME."
@@ -107,18 +110,18 @@
    (if (not filename)
        (message "Buffer '%s' is not visiting a file!" name)
      (if (get-buffer new-name)
-	 (message "A buffer named '%s' already exists!" new-name)
+         (message "A buffer named '%s' already exists!" new-name)
        (progn  (rename-file name new-name 1)  (rename-buffer new-name)  (set-visited-file-name new-name)  (set-buffer-modified-p nil))))))
 
 (defun revert-all-buffers()
    "Refreshes all open buffers from their respective files."
    (interactive)
    (let* ((list (buffer-list))
-	  (buffer (car list)))
+          (buffer (car list)))
      (while buffer
        (when (buffer-file-name buffer)
-	 (set-buffer buffer)
-	 (revert-buffer t t t))
+         (set-buffer buffer)
+         (revert-buffer t t t))
        (setq list (cdr list))
        (setq buffer (car list))))
    (message "done."))
@@ -134,7 +137,7 @@
    defaults at 32."
   (interactive)
   (let ((char-set (or char-set
-		      "1234567890abcdefghijklmnopqrstyvwxyz")))
+                      "1234567890abcdefghijklmnopqrstyvwxyz")))
     (dotimes (i (or len 32))
       (insert (elt char-set (random (length char-set)))))))
 
