7d648630a3d8381e42e82ebde5df88bc3c4fba65
dan <dan@sateda.(none)>
2012-12-14 05:33:29 -0500

    Switching to autoload.

--- .emacs.d/config/geben-config.el
+++ .emacs.d/config/geben-config.el
@@ -1,4 +1,4 @@
-(require 'geben)
+(autoload 'geben "geben" t)
 
 (custom-set-variables
  '(geben-dbgp-feature-list (quote ((:set max_data 32768) (:set max_depth 1) (:set max_children 1024) (:get breakpoint_types geben-dbgp-breakpoint-store-types))))
@@ -9,9 +9,9 @@
 (defun kill-proxy()
   "Adds a hook to call geben-proxy-end before killing emacs when in geben mode."
   (add-hook 'kill-emacs-hook
-	    (lambda()
-	      (interactive "P")
-	      (call-interactively 'geben-proxy-end))))
+            (lambda()
+              (interactive "P")
+              (call-interactively 'geben-proxy-end))))
 
 (add-hook 'geben-mode-hook 'kill-proxy)
 
