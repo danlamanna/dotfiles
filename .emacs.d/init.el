67f1891439f492fdd84f442fdb7ced9451d5485c
dan <dan@sateda.(none)>
2012-12-14 05:33:19 -0500

    Switching to autoloads, much more sane.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,37 +5,32 @@
   (normal-top-level-add-to-load-path '("."))
   (normal-top-level-add-subdirs-to-load-path))
 
-(require 'ace-jump-mode)
-(require 'assembla-mode)
-(require 'color-theme)
-(require 'expand-region)
-;(require 'geben)
-(require 'inline-string-rectangle)
-(require 'iy-go-to-char)
-(require 'magit)
-(require 'magit-svn)
-(require 'mark-more-like-this)
-(require 'restclient)
-(require 'rinari)
-(require 'theme-config)
-(require 'uniquify)
-;(require 'w3m)
-(require 'wordpress-mode)
-(require 'yasnippet)
+(autoload 'assembla "assembla-mode" t)
+(autoload 'ace-jump-word-mode "ace-jump-mode" t)
+(autoload 'ace-jump-char-mode "ace-jump-mode" t)
+(autoload 'er/expand-region "expand-region" t)
+(autoload 'inline-string-rectangle "inline-string-rectangle" t)
+(autoload 'iy-go-to-char "iy-go-to-char" t)
+(autoload 'iy-go-to-char-backward "iy-go-to-char" t)
+(autoload 'magit-status "magit" t)
+(autoload 'magit-svn-mode "magit-svn" t)
+(autoload 'mark-more-like-this "mark-more-like-this" t)
+(autoload 'restclient-mode "restclient" t)
+(autoload 'rinari-minor-mode "rinari" t)
+(autoload 'wordpress-mode "wordpress-mode" t)
 
+(require 'assembla-mode)
+(require 'theme-config)
 (require 'emacs-config)
-;(require 'geben-config)
+(require 'geben-config)
 (require 'ido-config)
 (require 'keymap-config)
 (require 'modeline-config)
 (require 'modes-config)
 (require 'uniquify-config)
 (require 'yasnippet-config)
+
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(asl/cache-enabled t)
  '(browse-url-browser-function (quote w3m-browse-url))
  '(geben-dbgp-feature-list (quote ((:set max_data 32768) (:set max_depth 1) (:set max_children 1024) (:get breakpoint_types geben-dbgp-breakpoint-store-types))))
@@ -44,10 +39,7 @@
  '(indent-tabs-mode nil)
  '(php-completion-file (expand-file-name (format "%s/etc/php-completion.txt" emacs-config-dir)))
  '(php-manual-path (expand-file-name (format "%s/etc/php-manual" emacs-config-dir))))
+
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(flymake-errline ((t (:background "brightblack"))))
  '(magit-item-highlight ((t (:inherit default)))))
