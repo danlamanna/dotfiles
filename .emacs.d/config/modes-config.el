28201dfa5ce46d63bc83c916e8ebb834f8de494b
Dan LaManna <dan.lamanna@gmail.com>
2013-01-06 17:10:28 -0500

    Cleaning up modes config.

--- .emacs.d/config/modes-config.el
+++ .emacs.d/config/modes-config.el
@@ -1,12 +1,10 @@
-; PHP
+;; PHP
 (custom-set-variables
  '(php-manual-path (expand-file-name (format "%s/etc/php-manual" emacs-config-dir)))
  '(php-completion-file (expand-file-name (format "%s/etc/php-completion.txt" emacs-config-dir))))
 
 (autoload 'php-mode "php-mode" "Major mode for PHP." t)
 (add-to-list 'auto-mode-alist '("\\.\\(php\\|phtml\\)\\'" . php-mode))
-
-(add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
 
 (require 'flymake)
 (add-hook 'php-mode-hook 'flymake-mode-on)
@@ -20,10 +18,6 @@
                (modes	. '(php-mode))
                (tab-stop)))
 
-;(add-hook 'before-save-hook (lambda()
-;                             (if (string-equal mode-name "PHP")
-;                                 (align (point-min) (point-max)))))
-
 (add-hook 'php-mode-hook '(lambda()
                             (require 'wordpress-mode)
                             (if (wp/exists)
@@ -33,13 +27,17 @@
                             (define-key php-mode-map (kbd "C-c C-f") 'php-search-local-documentation)
                             (define-key php-mode-map (kbd "<backtab>") 'php-complete-function)))
 
+;; Python
 (autoload 'django-html-mumamo-mode "~/.emacs.d/lib/nxhtml/autostart.el")
 (setq auto-mode-alist
       (append '(("\\.djhtml?$" . django-html-mumamo-mode)) auto-mode-alist))
 (setq mumamo-background-colors nil)
 (add-to-list 'auto-mode-alist '("\\.djhtml$" . django-html-mumamo-mode))
 
-; C
+;; Ruby
+(add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
+
+;; C
 (defun compile-or-recompile()
   (interactive)
   (if (get-buffer "*compilation*")
