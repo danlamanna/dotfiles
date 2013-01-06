0868ee88629c354aae7b93d7e892842d46f8f8e6
Dan LaManna <dan.lamanna@gmail.com>
2013-01-06 17:33:57 -0500

    Changing magit-svn to actually run hooks for magit-svn-mode, and use a hook to run it, rather than advice.

--- .emacs.d/config/magit-config.el
+++ .emacs.d/config/magit-config.el
@@ -1,3 +1,4 @@
+;; Advice/Functions
 (defadvice magit-status (around magit-fullscreen activate)
   (window-configuration-to-register :magit-fullscreen)
   ad-do-it
@@ -9,6 +10,13 @@
   (kill-buffer)
   (jump-to-register :magit-fullscreen))
 
+;; Hooks
+(add-hook 'magit-mode-hook (lambda()
+                             (require 'magit-svn)
+                             (if (magit-svn-get-ref-info)
+                                 (magit-svn-mode))))
+
+;; Keymaps
 (define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
 
 (provide 'magit-config)
