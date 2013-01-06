89d3bbf48dc8c43c91cc6a7942e0c487fd8c3e14
Dan LaManna <dan.lamanna@gmail.com>
2013-01-06 17:09:55 -0500

    Beginnings of figuring out geben debugger garbage.

--- .emacs.d/config/geben-config.el
+++ .emacs.d/config/geben-config.el
@@ -1,18 +1,14 @@
-(autoload 'geben "geben" t)
-
 (custom-set-variables
  '(geben-dbgp-feature-list (quote ((:set max_data 32768) (:set max_depth 1) (:set max_children 1024) (:get breakpoint_types geben-dbgp-breakpoint-store-types))))
  '(geben-dbgp-redirect-buffer-init-hook nil)
  '(geben-temporary-file-directory "~/.emacs.d/tmp/geben"))
 
+;; Fixing complications that occur with alive (or dead) dbgp listeners
+;(defadvice geben-proxy-end(around quietly-kill-proxy activate)
+ ; (ignore-errors
+  ;  ad-do-it))
 
-(defun kill-proxy()
-  "Adds a hook to call geben-proxy-end before killing emacs when in geben mode."
-  (add-hook 'kill-emacs-hook
-            (lambda()
-              (interactive "P")
-              (call-interactively 'geben-proxy-end))))
-
-(add-hook 'geben-mode-hook 'kill-proxy)
+;(add-hook 'kill-emacs-hook (lambda()
+ ;                            (call-interactively 'geben-proxy-end)))
 
 (provide 'geben-config)
