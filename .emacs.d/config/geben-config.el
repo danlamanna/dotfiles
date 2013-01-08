a1f6c53740c0144dd21ec1ebc786c71b0ba1395c
Dan LaManna <dan.lamanna@gmail.com>
2013-01-08 16:44:10 -0500

    Adding todo and ajax disable debug for geben.

--- .emacs.d/config/geben-config.el
+++ .emacs.d/config/geben-config.el
@@ -14,7 +14,8 @@
 
 (defun geben-single-or-proxy()
   "Tries calling geben, if it throws an error because it needs to use
-   `geben-proxy', it tries that."
+   `geben-proxy', it tries that.
+   TODO: make it toggle.."
   (interactive)
   (condition-case nil
       (geben)
@@ -26,3 +27,15 @@
 (add-hook 'kill-emacs-hook 'geben-safely-end-proxy)
 
 (provide 'geben-config)
+
+
+(setq geben-disable-ajax-requests t)
+
+(add-hook 'geben-session-enter-hook 'geben-ignore-ajax-requests)
+
+(defun geben-ignore-ajax-requests(session)
+  (when geben-disable-ajax-requests
+    (let* ((php-is-ajax "(int)(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest')")
+           (is-ajax-result (format "%s" (geben-eval-expression php-is-ajax))))
+      (when (string-equal "result: 1" is-ajax-result) ;; is an aja request
+        (geben-quit-window)))))
