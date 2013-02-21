e02ed8ec7f9c6dc344ef3b93143a09e384617b8e
dan <dan@archlantea.home>
2013-02-21 11:44:38 -0500

    IDO IN ALL OF THE THINGS.

--- .emacs.d/config/ido-config.el
+++ .emacs.d/config/ido-config.el
@@ -29,6 +29,7 @@
         (let ((ido-ubiquitous-enable-compatibility nil))
           ad-do-it))))
 
+(ido-ubiquitous-use-new-completing-read geben-find-file 'geben)
 (ido-ubiquitous-use-new-completing-read webjump 'webjump)
 (ido-ubiquitous-use-new-completing-read yas/expand 'yasnippet)
 (ido-ubiquitous-use-new-completing-read yas/visit-snippet-file 'yasnippet)
