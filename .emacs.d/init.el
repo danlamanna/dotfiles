e5390410486186b31e563c238d4f07a5e6a24e21
dan <dan@lantea.(none)>
2014-01-22 06:43:19 -0500

    All of the things.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,11 +1,7 @@
 (defvar emacs-config-dir (expand-file-name "~/.emacs.d"))
 (defvar emacs-tmp-dir    (expand-file-name (concat emacs-config-dir "/" "tmp")))
 
-(add-to-list 'load-path (concat emacs-config-dir "/lib/org-8.2.1/lisp"))
-(add-to-list 'load-path (concat emacs-config-dir "/lib/org-8.2.1/contrib/list"))
-
 (require 'org)
 (require 'ob-tangle)
 
 (org-babel-load-file (concat emacs-config-dir "/emacs.org"))
-(put 'downcase-region 'disabled nil)
