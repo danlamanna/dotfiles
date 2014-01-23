1a369272600ce623d3e7181986ebeaf8cbda2586
dan <dan@crunchbang.(none)>
2014-01-22 20:26:31 -0500

    Adds new org-mode.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,11 @@
 (defvar emacs-config-dir (expand-file-name "~/.emacs.d"))
 (defvar emacs-tmp-dir    (expand-file-name (concat emacs-config-dir "/" "tmp")))
 
+
+(add-to-list 'load-path (concat emacs-config-dir "/lib/org-8.2.5g/lisp"))
+(add-to-list 'load-path (concat emacs-config-dir "/lib/org-8.2.5g/contrib/list"))
+
+
 (require 'org)
 (require 'ob-tangle)
 
