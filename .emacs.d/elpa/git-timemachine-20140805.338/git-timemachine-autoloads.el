71b78ed80dfdf877e8c9538bace85d51f3fad75a
Dan LaManna <dan.lamanna@gmail.com>
2014-08-07 11:34:38 -0400

    Updates packages.

---
+++ .emacs.d/elpa/git-timemachine-20140805.338/git-timemachine-autoloads.el
@@ -0,0 +1,27 @@
+;;; git-timemachine-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "git-timemachine" "git-timemachine.el" (21475
+;;;;;;  40036 0 0))
+;;; Generated autoloads from git-timemachine.el
+
+(autoload 'git-timemachine "git-timemachine" "\
+Enable git timemachine for file of current buffer.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("git-timemachine-pkg.el") (21475 40036
+;;;;;;  23493 103000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; git-timemachine-autoloads.el ends here
---
+++ .emacs.d/elpa/git-timemachine-20140805.338/git-timemachine-pkg.el
@@ -0,0 +1,1 @@
+(define-package "git-timemachine" "20140805.338" "Walk through git revisions of a file" '((cl-lib "0.5")) :url "https://github.com/pidu/git-timemachine" :keywords ("git"))
