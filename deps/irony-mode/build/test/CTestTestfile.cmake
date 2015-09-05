4aa220932acaf1f37999a5106c7efd6c4dd40ea7
Dan LaManna <me@danlamanna.com>
2015-09-05 13:19:09 -0400

    Out with the old

--- deps/irony-mode/build/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dan/src/irony-mode/test
-# Build directory: /home/dan/src/irony-mode/build/test
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(elisp)
--- deps/irony-mode/build/test/elisp/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dan/src/irony-mode/test/elisp
-# Build directory: /home/dan/src/irony-mode/build/test/elisp
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(irony-el "/usr/local/bin/emacs" "-batch" "-l" "/home/dan/src/irony-mode/test/elisp/test-irony.el" "-f" "ert-run-tests-batch-and-exit")
-ADD_TEST(irony-cdb-el "/usr/local/bin/emacs" "-batch" "-l" "/home/dan/src/irony-mode/test/elisp/test-irony-cdb.el" "-f" "ert-run-tests-batch-and-exit")
