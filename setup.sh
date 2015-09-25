33bafe13139edaa5bfb1e98195b4c5efc3683f6b
Dan LaManna <me@danlamanna.com>
2015-09-24 22:13:28 -0400

    Add htop config

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
-ln -s ~/dotfiles/.gitconfig        ~/.gitconfig
-ln -s ~/dotfiles/.tmux.conf        ~/.tmux.conf
-ln -s ~/dotfiles/.Xmodmap          ~/.Xmodmap
-ln -s ~/dotfiles/.zshrc            ~/.zshrc
+mkdir -p ~/.config/htop
+ln -s ~/dotfiles/.config/htop/htoprc ~/.config/htop/htoprc
+
+ln -s ~/dotfiles/.gitignore_global   ~/.gitignore_global
+ln -s ~/dotfiles/.gitconfig          ~/.gitconfig
+ln -s ~/dotfiles/.tmux.conf          ~/.tmux.conf
+ln -s ~/dotfiles/.Xmodmap            ~/.Xmodmap
+ln -s ~/dotfiles/.zshrc              ~/.zshrc
