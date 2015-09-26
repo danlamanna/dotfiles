024565c837130e4344f237d85f2e58ff034dd943
Dan LaManna <me@danlamanna.com>
2015-09-25 22:15:53 -0400

    Add systemd emacs

--- setup.sh
+++ setup.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+# htop
 mkdir -p ~/.config/htop
 ln -s ~/dotfiles/.config/htop/htoprc ~/.config/htop/htoprc
 
+# emacs
+mkdir -p ~/.config/systemd/user
+ln -s ~/dotfiles/.config/systemd/user/emacs.service ~/.config/systemd/user/emacs.service
+
+if ! systemctl is-enabled --user emacs; then
+    systemctl enable --user emacs
+fi
+
+# rest
 ln -s ~/dotfiles/.gitignore_global   ~/.gitignore_global
 ln -s ~/dotfiles/.gitconfig          ~/.gitconfig
 ln -s ~/dotfiles/.tmux.conf          ~/.tmux.conf
