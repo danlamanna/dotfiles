a23042bce80b01487083282153800fcd923dcc67
Dan LaManna <me@danlamanna.com>
2016-08-09 09:44:24 -0400

    Add per host xinitrc

---
+++ per-host/lantea/xinitrc/xrandr.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+/usr/bin/xrandr --output DP-3 --off --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DVI-I-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-2 --off --output DP-1 --off
